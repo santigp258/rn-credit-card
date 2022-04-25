d8ebaa8754e9487be27209c4821dd5ed92928208
santigp258 <santigp258@gmail.com>
2022-04-25 13:21:16 -0500

    enable some exports from index and update package version

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import CreditCardForm from './components/CreditCardForm'
 export { default as Button } from './components/Button'
 export { default as Card } from './components/Card'
+export { default as CardIcon } from './components/CardIcon'
+export { default as Conditional } from './components/Conditional'
+export { default as FormCard } from './components/FormCard'
+export { default as FormTextField } from './components/FormTextField'
 export { default as LibraryContext } from './LibraryContext'
 export { getTranslations } from './utils/translations'
+export {
+  cardNumberFormatter,
+  expirationDateFormatter,
+} from './utils/formatters'
 
-import { FormModel, CardFields } from './types'
+export { FormModel, CardFields, LibraryProps, Translations } from './types'
 
-export { CardFields, FormModel }
 export default CreditCardForm
